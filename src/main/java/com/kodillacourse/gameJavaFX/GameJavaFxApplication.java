bf8c627ec70904fd738832989d672616a341752a
Katarzyna Nabożny <knnabozny@gmail.com>
2021-11-03 20:52:09 +0100

    First commit

---
+++ src/main/java/com/kodillacourse/gameJavaFX/GameJavaFxApplication.java
@@ -0,0 +1,13 @@
+package com.kodillacourse.gameJavaFX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameJavaFxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameJavaFxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodillacourse/gameJavaFX/GameJavaFxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodillacourse.gameJavaFX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameJavaFxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
